5c0eafb0afed6b86866fa5610a5106565a61d8fe
kikkal-fairy <thelkouk1234@gmail.com>
2025-04-07 23:07:02 +0100

    Update Navigation.jsx
    Fixed Navigation Bar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -6,12 +6,10 @@ const Navigation = () => {
   return (
     <nav className="nav-bar">
       <Link to="/">Home</Link>
-      <Link to="/dashboard">Dashboard</Link>
-      <Link to="/activity-detail">Activity Details</Link>
-      <Link to="/search">Search</Link>
-      <Link to="/resources">Resources</Link>
-      <Link to="/legal">Legal</Link>
-      <Link to="/account">Account</Link>
+      <Link to="/aboutus">About Us</Link>
+      <Link to="/partnerOrg">Partner Orginisations</Link>
+      <Link to="/career">Career Guidance</Link>
+      <Link to="/applicationGuidance">Application Guidance</Link>
     </nav>
   );
 };
