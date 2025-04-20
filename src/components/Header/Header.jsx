749c8e59ef9afc04f00d7c5507f97e47f1e8ad26
LeoRojboonthueng <40651296@live.napier.ac.uk>
2025-04-20 03:32:06 +0100

    Cleaned up nav bar and made dynamic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,6 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import NESLogo from '../../assets/nes-logo.png';
-// import './Header.css';
-
-
-
-// const Header = () => {
-//   return (
-//     <header className="header">
-//       <div className="header-container">
-//         <img src={NESLogo} alt="NES Logo" className="logo" style={{ height: "150px", width: "auto" }} />
-
-//         <div className="header-text">
-//           <h1>Outreach Hub</h1>
-//           <h4>Inspiration and preparation activities for medical school applicants in Scotland</h4>
-//         </div>
-        
-//         <div className="header-buttons">
-//           <Link to="/login" className="login-button">
-//             <button>Sign In</button>
-//           </Link>
-//           <Link to="/register" className="register-button">
-//             <button>Register As Provider</button>
-//           </Link>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// };
-
-// export default Header;
 import React from 'react';
 import { Link } from 'react-router-dom';
-import NESLogo from '../../assets/nes-logo.png'; // Replace with your logo path
+import NESLogo from '/src/assets/nes-logo.png';
 import './Header.css';
 
 const Header = () => {
